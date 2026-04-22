ada0a82e06a1b89fe41450026df1ea01a057ffff
JeamCMR <tutomx16@gmail.com>
2026-04-21 22:01:02 -0500

    Se realiza ajustes

--- src/main/java/Aritmitica.java
+++ src/main/java/Aritmitica.java
@@ -1,5 +1,12 @@
 public class Aritmitica {
 
+    //Constructores
+    //Por defecto siempre crea un contructor vacio sin necesidad de crearlo
+     public Aritmitica(){
+         System.out.println("Constructor Vacio");
+     }
+
+
     public int sumar (int a, int b){
         int resultado = a + b;
         return resultado;
