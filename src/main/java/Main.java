3d291b6cddf8c439071a9d2bd010719ac45c0fdb
JeamCMR <tutomx16@gmail.com>
2026-04-21 21:46:30 -0500

    Primeros pasos

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main (String[] args){
+        Aritmitica suma =  new Aritmitica();
+        int resultado = suma.sumar(100021,21230);
+        System.out.println("Resultado: " + resultado);
+    }
+}
