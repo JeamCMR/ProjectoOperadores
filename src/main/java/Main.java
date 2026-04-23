bfc43fda5c3f668787fa4b60fa2b8ce4a9cb6860
JeamCMR <tutomx16@gmail.com>
2026-04-22 22:39:24 -0500

    Alcance de variables, this, variables de clase y locales

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,5 +4,19 @@ public class Main {
       //Varriables locales
         int operandoA = 6;
         int operadoB = 2;
+
+        //Objeto de clase Aritmetica
+        Aritmitica objeto1 = new Aritmitica(operandoA,operadoB);
+        System.out.println("operandoA= " + operandoA);
+        System.out.println("operadoB= " + operadoB);
+
+        //Imprimir el resultado de la suma del metodo de la clase que tiene el objeto
+        System.out.println("Resultado de la suma: " + objeto1.sumar());
+        System.out.println("Resultado de la resta: " + objeto1.restar());
+        System.out.println("Resultado de la multiplicacion: " + objeto1.multiplicar());
+        System.out.println("Resultado de la Dividir: " + objeto1.dividir());
     }
 }
+
+//Nota Los paramentros son las variables que se definen en un metodo o funciones sumar (int a, int b)
+//Los argumentos son los valores reales que van a tener los parametros ejemplo sumar(12,3)
