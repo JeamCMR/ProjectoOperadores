34b5a4ec6773d9b971eb5ec55d3e4deb1d337c7e
JeamCMR <tutomx16@gmail.com>
2026-04-22 22:18:05 -0500

    Se crear constructor con argumentos

--- src/main/java/Aritmitica.java
+++ src/main/java/Aritmitica.java
@@ -4,12 +4,16 @@ public class Aritmitica {
     int b;
     //Constructores
     //Por defecto siempre crea un contructor vacio sin necesidad de crearlo
-     public Aritmitica(){
+    /* public Aritmitica(){
          a = 20;
          b = 30;
-     }
+     }*/
     //Constrctor con Parametros
-
+    public  Aritmitica(int arg1, int arg2){
+         a=arg1;
+         b=arg2;
+        System.out.println("Se ejecuta el constructor con 2 argumentos");
+    }
 
     public int sumar (){
         int resultado = a + b; //si quitamos los paramtros, se empiezan a usar los atributos de la clase
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,12 @@
 public class Main {
 
     public static void main (String[] args){
-        Aritmitica suma =  new Aritmitica();
+        Aritmitica suma =  new Aritmitica( 2, 3);
         int resultado = suma.sumar();
         System.out.println("Resultado: " + resultado);
+
+        Aritmitica suma2 = new Aritmitica(10,21);
+        System.out.println("Resultado 2: " + suma2.sumar());
+
     }
 }
